28b196c7bf49bf5430fddfae2964bf9261e70d44
Chitra Shrivastava <142480818+chitrashrivastava@users.noreply.github.com>
2024-05-15 22:08:23 +0530

    Update database.js

--- models/database.js
+++ models/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.connectDB = async (req, res) => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/internshala', {
+    await mongoose.connect('mongodb://127.0.0.1:27017/internshala', {
     });
     console.log('Connected to MongoDB');
   } catch (err) {
